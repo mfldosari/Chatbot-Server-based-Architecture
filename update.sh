f2844f29f980da58c6f789c3cd972293dde90453
Mohammed Aldosari <91144751+mfldosari@users.noreply.github.com>
2025-05-01 14:42:39 +0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -11,9 +11,9 @@ rm -r -f stage6.5/
 if [ -d "$APP_DIR" ]; then
     sudo -u azureuser bash -c "cd $APP_DIR && git pull origin $BRANCH"
 else
-    sudo -u azureuser git clone -b $BRANCH "https://$GITHUB_TOKEN@$github.com/mfldosari/stage6.5.git" "$APP_DIR"
+    sudo -u azureuser git clone -b $BRANCH "https://$GITHUB_TOKEN@github.com/mfldosari/stage6.5.git" "$APP_DIR"
 fi
-source /home/azureuser/env/bin/activate
+source /home/azureuser/stage6.5/myenv/bin/activate
 # Install dependencies
 sudo -u azureuser /home/azureuser/stage6.5/myenv/bin/pip install --upgrade pip
 sudo -u azureuser /home/azureuser/stage6.5/myenv/bin/pip install -r "$APP_DIR/requirements.txt"
