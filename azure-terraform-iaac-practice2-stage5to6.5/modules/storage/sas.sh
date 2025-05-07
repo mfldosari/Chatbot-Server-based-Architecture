41ca1032f64918ddccfd264f04194ad19f883dfa
jax wood <jax.wood.m@gmail.com>
2025-05-07 09:49:21 +0300

    change the sas shell script contents

--- azure-terraform-iaac-practice2-stage5to6.5/modules/storage/sas.sh
+++ azure-terraform-iaac-practice2-stage5to6.5/modules/storage/sas.sh
@@ -1,12 +1,35 @@
 #!/bin/bash
-az storage blob generate-sas \
-  --account-name storageaccount7603 \
-  --container-name chatbot \
-  --name pdf_store \
-  --permissions acdrw \
-  --expiry 2025-05-05T00:00:00Z \
+# Accept arguments
+STORAGE_ACCOUNT=$1
+CONTAINER_NAME=$2
+VAULT_NAME=$3
+SECRET_NAME=$4
+START=$5
+EXPIRY=$6
+
+az role assignment create \
+  --assignee <object id> \
+  --role "Storage Blob Data Contributor" \
+  --scope "/subscriptions/ac2d458e-bde5-440e-86dc-d8312f7cb11a/resourceGroups/tr-stage6-ma-proj/providers/Microsoft.Storage/storageAccounts/$STORAGE_ACCOUNT"
+
+
+SAS_TOKEN=$(az storage container generate-sas \
+  --account-name "$STORAGE_ACCOUNT" \
+  --name "$CONTAINER_NAME" \
+  --permissions racwdli \
+  --expiry 2025-05-08T00:00:00Z \
   --auth-mode login \
   --as-user \
-  --full-uri > ./sas_token.txt
-  
-  az keyvault secret set --vault-name chatbot13-keyvault --name PROJ-AZURE-STORAGE-SAS-URL --value $(cat ./sas_token.txt)
+  --https-only \
+  --output tsv)
+
+
+# Construct full SAS URL
+FULL_URL="https://${STORAGE_ACCOUNT}.blob.core.windows.net/${CONTAINER_NAME}?${SAS_TOKEN}"
+echo "$FULL_URL" > ./sas_url.txt
+
+# Store the SAS URL in Key Vault
+az keyvault secret set \
+  --vault-name "$VAULT_NAME" \
+  --name "$SECRET_NAME" \
+  --value "$FULL_URL"
